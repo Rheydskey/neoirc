66ae0ddfafb9321d870afd801bdd542155077f14
Rheydskey <matgag02@gmail.com>
2021-01-03 16:12:05 +0100

    :art: Add a "connected" column

--- src/func/user/db/update.rs
+++ src/func/user/db/update.rs
@@ -14,4 +14,18 @@ pub async fn update_token(token: String, id: i32) -> Result<(), sqlx::Error> {
     }
 }
 
-pub async fn connect_user_by_token() {}
+pub async fn update_connect_status_by_token(status: bool,token: String) -> Result<(), sqlx::Error>{
+    let mut conn = connect().await;
+    match query("UPDATE user SET connected = $1 WHERE token = $2").bind(status).bind(token).execute(&mut conn).await {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e)
+    }
+}
+
+pub async fn remove_token(token: String) -> Result<(), sqlx::Error>{
+    let mut conn = connect().await;
+    match query("UPDATE user SET token = $1 WHERE token = $2").bind("").bind(token).execute(&mut conn).await {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e)
+    }
+}
